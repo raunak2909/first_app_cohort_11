395727033333914f0f3b043a6a01946f9daf40f6
wscubegoodtogo-beep <wscube.goodtogo@gmail.com>
2026-07-01 21:31:46 +0530

    conditional programming and handling inputs via console

---
+++ lib/cond_prog_exp.dart
@@ -0,0 +1,35 @@
+import 'first_prog.dart';
+
+
+void main(){
+
+  print("Enter your name: ");
+
+  String name = getStrValue();
+
+  print("Enter your age: ");
+
+  int age = getIntValue();
+
+
+  /// ==, !=, a>b, a<b, >=, <=
+  if(age>=18){
+    print("$name, You are eligible to vote!!");
+  } else {
+    print("$name, You are not eligible to vote!!");
+  }
+
+  ///1. Find if the given no is ODD or EVEN.
+  ///2. Find the greatest of 3 nos.
+  ///3. Find the smallest of 3 nos.
+  ///4. Find if the given year is LEAP year or not.
+  ///5. Write a program which asks about no1 and no2
+  ///   then asks about the choice
+  ///   from 1 for Addition
+  ///   2 for substraction
+  ///   3 for multiply
+  ///   4 for division
+  ///   5 for modulus
+  ///   then act accordingly!!
+
+}
--- lib/first_prog.dart
+++ lib/first_prog.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 void main(){
 
   // int? no1;
@@ -16,22 +18,54 @@ void main(){
   /*no1 = 11;
   print((no1 ?? 0)*10);*/
 
-  int result = add(11,21);
 
-  int result2 = add(result,400);
+  /// write
+  print("Hi, Enter your name: ");
 
-  print(result2);
+  ///read
+  String name = getStrValue(); /// "null"
+
+
+  /// 5 + 6 = 11
+  /// "5" + "6" = "56"
+
+  ///print("Hello $name, Welcome to Dart!!");
+  stdout.writeln("Hello $name, Welcome to Dart!!");
+  stdout.write("Enter no1 : ");
+  int no1 = getIntValue(); /// "56"+"21" -> "5621", 56+21 -> 77
+
+  stdout.write("Enter no2 : ");
+  int no2 = getIntValue();
+
+  print("sum : ${add(no1, no2)}");
+  print("diff : ${sub(no1, no2).abs()}");
+  print("multi : ${multi(no1, no2)}");
+  print("div : ${div(no1, no2)}");
+  print("rem : ${mod(no1, no2)}");
+
 }
 
-
+/// create a program for Addition
 int add(int no1, int no2){
+  ///(a+b) pow 2
   int sum = no1+no2;
   return sum;
 }
 
-/// create a program for Addition
+
 /// create a program for Subtraction
+int sub(int no1, int no2){
+  return no1-no2;
+}
 /// create a program for Multiplication
+int multi(int no1, int no2) => no1*no2;
 /// create a program for Division
+//int div(int no1, int no2) => (no1/no2).toInt();
+//int div(int no1, int no2) => no1~/no2;
+num div(int no1, int no2) => no1/no2;
 /// create a program for Modulus (Remainder)
+int mod(int no1, int no2) => no1%no2;
 /// +, -, *, /, %
+
+String getStrValue() => stdin.readLineSync()!;
+int getIntValue() => int.parse(getStrValue());
